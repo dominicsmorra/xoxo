53e5f378c07c2f4eb0732b3db29163e4ae02d1f4
Victor Li <vli8@fordham.edu>
2018-07-05 15:12:57 -0400

    streak part

--- game/index.js
+++ game/index.js
@@ -1,6 +1,31 @@
-import {Map} from 'immutable'
+import { Map } from 'immutable'
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+export const MOVE = 'MOVE';
+
+export const move = (player, coord) => ({
+  type: MOVE,
+  player,
+  coord
+})
+
+function turn (current = 'X', action) {
+  if (action.type === MOVE) {
+    return current === 'X' ? 'O' : 'X'
+  }
+  return current
+}
+
+function board (board = Map(), {type, coord, player}) {
+  if (type === MOVE) {
+    return board.setIn(coord, player)
+  }
+  return board
+}
+
+
+export default function reducer(state = {}, action) {
+  return {
+    board: board(state.board, action),
+    turn: turn(state.turn, action)
+  }
 }
